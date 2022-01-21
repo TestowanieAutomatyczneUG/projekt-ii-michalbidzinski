dd069581b716546d7910a2352e4f7c83269d0528
Michał Bidziński <michal.bidzinski1337@gmail.com>
2022-01-21 23:18:19 +0100

    dodano klase klient

---
+++ src/client.py
@@ -0,0 +1,6 @@
+class Client:
+    def __init__(self):
+        self.create = "https://client.pl/add"
+        self.read = "https:/client.pl/get"
+        self.update = "https:/client.pl/update"
+        self.delete = "https:/client.pl/update"
